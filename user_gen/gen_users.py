aa65a234f628dd499a1e494e8e58cab5af1cc623
Dan Fedick <daniel.fedick@hashicorp.com>
2023-04-27 20:59:10 -0400

    Add Python user generator

---
+++ user_gen/gen_users.py
@@ -0,0 +1,30 @@
+import json
+import random
+from faker import Faker
+
+fake = Faker()
+
+job_types = ['manager', 'product-manager' , 'executive', 'devops-engineer', 'devsecops-engineer', 'developer', "customer-success"]
+
+
+users = []
+
+for _ in range(50):
+    user = {
+        "name": fake.name(),
+        "uuid": fake.uuid4(),
+        "address": fake.address().replace("\n", ", "),
+        "phone": fake.phone_number(),
+        "user_agent": fake.user_agent(),
+        "company": "OpenGovCo",
+        "email": fake.email(),
+        "team": random.choice(job_types),
+        "location": fake.city(),
+        "credit_card": fake.credit_card_number(card_type='mastercard'),
+        "social_security": fake.ssn(),
+    }
+    users.append(user)
+
+with open("users.json", "w") as f:
+    json.dump(users, f, indent=2)
+
